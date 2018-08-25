7eb175d8db85f2ee9140b8f5a6f8876e0e3da5da
Ben Tucker <nenofite@gmail.com>
2018-08-25 14:20:54 -0700

    Implement MastodonStream

--- src/tootsee/ports/mastodon_stream.cr
+++ src/tootsee/ports/mastodon_stream.cr
@@ -3,5 +3,25 @@ module Tootsee
     abstract class MastodonStream
       abstract def listen(&block : Mastodon::Entities::Notification -> Void)
     end
+
+    class MastodonStreamI < MastodonStream
+      def initialize(@config : Config); end
+
+      def listen(&block : Mastodon::Entities::Notification -> Void)
+        loop do
+          streaming_client = Mastodon::Streaming::Client.new(
+            url: @config[:masto_url],
+            access_token: @config[:access_token],
+          )
+          streaming_client.user do |obj|
+            if obj.is_a?(Mastodon::Entities::Notification)
+              yield obj
+            end
+          end
+        rescue ex
+          puts("Exception in listen loop: #{ex}")
+        end
+      end
+    end
   end
 end
