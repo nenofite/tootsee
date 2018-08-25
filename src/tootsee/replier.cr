a2502e3920abc89ab221f148149c60f2e9a5ccc8
Ben Tucker <nenofite@gmail.com>
2018-08-25 16:29:42 -0700

    Include full user in reply

--- src/tootsee/replier.cr
+++ src/tootsee/replier.cr
@@ -6,8 +6,9 @@ module Tootsee
     # Send the given text in reply to the given mention.
     def reply(text : String, in_reply_to : Listener::Mention)
       # Send a reply
+      reply_text = "@#{in_reply_to[:full_user]} #{text}"
       @client.toot(
-        text: text,
+        text: reply_text,
         in_reply_to_id: in_reply_to[:id],
         spoiler_text: in_reply_to[:spoiler_text],
         visibility: in_reply_to[:visibility],
