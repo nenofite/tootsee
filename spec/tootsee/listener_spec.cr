3f398bdabd9c42cab5d08ac51b605bce2974fc88
Ben Tucker <nenofite@gmail.com>
2018-08-25 13:43:10 -0700

    Finish listener specs

--- spec/tootsee/listener_spec.cr
+++ spec/tootsee/listener_spec.cr
@@ -15,9 +15,24 @@ module Tootsee
       end
 
       it "discards non-mentions" do
+        notifs = [fake_mention, fake_follow, fake_mention]
+        stream = MockMastodonStream.new(notifs)
+
+        called = 0
+        Tootsee::Listener.new(stream).listen do |mention|
+          called += 1
+        end
+        called.should eq 2
       end
 
       it "strips HTML from the mention" do
+        notifs = [fake_mention]
+        notifs[0].status.should be "<p>Hello world.</p>"
+        stream = MockMastodonStream.new(notifs)
+
+        Tootsee::Listener.new(stream).listen do |mention|
+          mention[:text].should be "Hello world."
+        end
       end
     end
   end
