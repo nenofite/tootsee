05b65d2e6275dea1c6a195e1fb53b02b7bdd6693
Ben Tucker <nenofite@gmail.com>
2018-08-25 16:28:16 -0700

    Test include full user in reply text

--- spec/tootsee/replier_spec.cr
+++ spec/tootsee/replier_spec.cr
@@ -11,25 +11,27 @@ module Tootsee
           id: "123",
           spoiler_text: "",
           visibility: "public",
+          full_user: "friend@example.com",
         }
         mention2 = {
           text: "say bye",
           id: "456",
           spoiler_text: "byeee",
           visibility: "direct",
+          full_user: "test",
         }
 
         replier.reply("Hello world", mention1)
         replier.reply("Goodbye", mention2)
         client.toots.should eq [
           {
-            text: "Hello world",
+            text: "@friend@example.com Hello world",
             in_reply_to_id: "123",
             spoiler_text: "",
             visibility: "public",
           },
           {
-            text: "Goodbye",
+            text: "@test Goodbye",
             in_reply_to_id: "456",
             spoiler_text: "byeee",
             visibility: "direct",
