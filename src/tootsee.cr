e157360127d618912e1e1521545faff0be80fe27
Ben Tucker <nenofite@gmail.com>
2018-08-27 22:14:47 -0700

    Toot the results of the caption

--- src/tootsee.cr
+++ src/tootsee.cr
@@ -21,20 +21,27 @@ module Tootsee
       access_token: ENV["ACCESS_TOKEN"],
       port: ENV["PORT"].to_i,
       azure_url: ENV["AZURE_URL"],
+      azure_key: ENV["AZURE_KEY"],
     }
 
     # Create ports
     stream = Ports::MastodonStreamI.new(config)
     client = Ports::MastodonClientI.new(config)
+    http_client = Ports::HTTPClientPortI.new
 
     # Create components
     listener = Listener.new(stream)
     replier = Replier.new(client)
+    captioner = Captioner.new(http_client, config)
 
     # Let's go 🎉
     listener.listen do |mention|
       puts("Received mention: #{mention}")
-      replier.reply("hi :3", mention)
+      # TODO For now we hardcode a URL of a corgi doing a sploot. Eventually
+      # this will be the URL from an image search.
+      caption = captioner.caption("https://i.pinimg.com/736x/ca/fc/f3/cafcf316846261671d6a3944838c180d--corgi-mix-young-ones.jpg")
+      puts("Caption: #{caption}")
+      replier.reply(caption, mention)
     end
   end
 end
