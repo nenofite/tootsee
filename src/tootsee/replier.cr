658b0262b37d3e93432d609672ff473a9c188b02
angus <angushtlam@gmail.com>
2018-09-08 17:55:59 -0400

    Implemented multithreading on listener

--- src/tootsee/replier.cr
+++ src/tootsee/replier.cr
@@ -7,12 +7,17 @@ module Tootsee
     def reply(text : String, in_reply_to : Listener::Mention)
       # Send a reply
       reply_text = "@#{in_reply_to[:full_user]} #{text}"
-      @client.toot(
-        text: reply_text,
-        in_reply_to_id: in_reply_to[:id],
-        spoiler_text: in_reply_to[:spoiler_text],
-        visibility: in_reply_to[:visibility],
-      )
+
+      begin
+        @client.toot(
+          text: reply_text,
+          in_reply_to_id: in_reply_to[:id],
+          spoiler_text: in_reply_to[:spoiler_text],
+          visibility: in_reply_to[:visibility],
+        )  
+      rescue ex : JSON::MappingError | JSON::ParseException
+        puts("Exception found for JSON parsing. Disregarding...")
+      end
     end
   end
 end
